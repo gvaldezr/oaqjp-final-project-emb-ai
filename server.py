385564abec39a635ee27256f4fe08010eb115fbe
gvaldezr <guillermo.valdez>
2024-07-31 14:36:45 -0400

    Final version

--- server.py
+++ server.py
@@ -1,18 +1,25 @@
+'''
+Function to call emotion analyzer 
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    ''' Analyzer '''
     text_to_analyse = request.args.get('textToAnalyze')
     res = emotion_detector(text_to_analyse)
     if res['dominant'] == 'none':
         return "Invalid text! Please try again!"
-    formated_res = f"anger:{res['anger']}, disgust:{res['disgust']}, fear:{res['fear']}, joy:{res['joy']}, sadness:{res['sadness']} "
-    return f"For the given statement, the system response is {formated_res}. The dominant emotion is {res['dominant']}"
+    formated_res = f"anger:{res['anger']}, disgust:{res['disgust']}, "
+    formated_res += f"fear:{res['fear']}, joy:{res['joy']}, sadness:{res['sadness']} "
+    res_dominant = f"The dominant emotion is {res['dominant']}"
+    return f"For the given statement, the system response is {formated_res}. {res_dominant}"
 
 @app.route("/")
 def render_index_page():
+    '''Render index '''
     return render_template('index.html')
 
 if __name__ == "__main__":
